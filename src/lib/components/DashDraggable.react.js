b4b15692be7cd7a6ee31f089a5f973cae7563f18
lcopey <laurent.copey@gmail.com>
2022-03-20 16:28:12 +0100

    Storing taskorder

--- src/lib/components/DashDraggable.react.js
+++ src/lib/components/DashDraggable.react.js
@@ -11,7 +11,38 @@ import Column from './column.jsx';
  * which is editable by the user.
  */
 export default class DashDraggable extends Component {
-    onDragEnd = () => {
+    onDragEnd = result => {
+        const { destination, source, draggableId } = result;
+
+        if (!destination) {
+        return;
+        }
+
+        if (
+        destination.droppableId === source.droppableId &&
+        destination.index === source.index
+        ) {
+        return;
+        }
+
+        const column = this.props.columns[source.droppableId];
+        const newTaskIds = Array.from(column.taskIds);
+        newTaskIds.splice(source.index, 1);
+        newTaskIds.splice(destination.index, 0, draggableId);
+
+        const newColumn = {
+        ...column,
+        taskIds: newTaskIds
+        };
+
+        const newProps = {
+        ...this.props,
+        columns: {...
+            this.props.columns,
+            [newColumn.id]: newColumn
+        }
+        };
+        this.props.setProps(newProps);
 
     };
     onDragStart = () => {
